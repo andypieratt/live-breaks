cc1848510baf76fc7ce2a1b3f4573b62a951bb1a
andypieratt <pieratt.aw@gmail.com>
2022-08-25 14:34:13 -0600

    started adding a bit of styling and trying to work on the animations for the login button to switch from reading login and signup

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import "./header.css";
+import { Button } from "@mui/material";
 
 export default function Header() {
   return (
-    <div>
-      <h3>LIVE-BREAKZ</h3>
+    <div className="header-container">
+      <h3 className="logo">LIVE-BREAKZ</h3>
+      <div className="auth-item-container">
+        <Button variant="contained" size="medium">
+          <div className="auth-item-wrap">
+            <div className="auth-item">Login</div>
+            <div className="auth-item">Signup</div>
+          </div>
+        </Button>
+      </div>
     </div>
   );
 }
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,1 +1,18 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Live-Breakz</h1>
+      <h4>
+        All your needs for sports cards and live breaking of packs and boxes
+      </h4>
+      <Button variant="contained" size="large">
+        Let's Go!
+      </Button>
+    </div>
+  );
+}
