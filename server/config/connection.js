3a3568ea90fd500947d25dffc12d8f9bc4dc9140
andypieratt <pieratt.aw@gmail.com>
2022-08-24 20:24:25 -0600

    switching server to MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,13 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const mongoose = require("mongoose");
 
-let sequelize;
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/super-sweet-react-chat",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
-module.exports = sequelize;
+module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,23 @@
-const path = require("path");
 const express = require("express");
-const session = require("express-session");
-const sequelize = require("./config/connection");
-//Initalizing app variable
-const app = express();
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: process.env.SECRET || "supersecretpassword",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-//Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(session(sess));
 
-//Listening
-sequelize.sync({ force: false }).then(() => {
-  http.listen(PORT, () =>
-    console.log(`Now listening http://localhost:${PORT}/`)
-  );
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.use(routes);
+
+db.once("open", () => {
+  const http = app.listen(PORT, () => {
+    console.log(`Now running on ${PORT}`);
+  });
 });
